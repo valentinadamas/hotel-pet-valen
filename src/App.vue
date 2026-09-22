03f406bb1fd32abb98fe990763ca1067aa6f3426
valentinadamas <damasvalentina9@gmail.com>
2026-09-22 11:49:50 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import DefaultLayout from './components/layout/DefaultLayout.vue';
+import AppNavbar from './components/layout/AppNavbar.vue';
+import AppSidebar from './components/layout/AppSidebar.vue';
+
 </script>
 
 <template>
-  <RouterView />
+  <DefaultLayout/>
+  <RouterView/>
+  <AppNavbar/>
+  <AppSidebar/>
 </template>
